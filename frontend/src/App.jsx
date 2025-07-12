1d0418dcb59b741e117beacf2dbae01b47059cb5
ermiayn3962 <113037878+ermiayn3962@users.noreply.github.com>
2025-07-12 10:52:27 -0400

    Frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@
 
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage'
-// import HomePage from './pages/homePage';
+import HomePage from './pages/homePage';
 import { Routes, Route } from 'react-router-dom'
 // import { SignIn, SignUp, SignedIn, SignedOut, RedirectToSignIn, Navigate } from "@clerk/clerk-react";
 
@@ -15,6 +15,7 @@ function App() {
   return (
     <Routes>
       {/* <Route path='/' element={<Navigate to='/sign-in'/>}/> */}
+      <Route path='/' element={<HomePage/>}/>
       <Route path="/sign-in/" element={<SignInPage />} />
       <Route path="/sign-up/" element={<SignUpPage />} />
       {/* <Route path="/home" element={
